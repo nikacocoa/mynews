7235834caa05cf5f73019cfdc4adb77e0db31d25
EC2 Default User <ec2-user@ip-172-31-13-196.ap-southeast-2.compute.internal>
2023-12-26 16:17:52 +0000

    PHP/Lalavel5

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+
+@section('title', 'プロフィール編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @method('PUT')
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile->name }}" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" name="gender" value="男性" {{ $profile->gender === '男性' ? 'checked' : '' }}> 男性
+                            <input type="radio" name="gender" value="女性" {{ $profile->gender === '女性' ? 'checked' : '' }}> 女性
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows
